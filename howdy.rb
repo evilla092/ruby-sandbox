9ca69136e383e9d4128b0d4f5374c60229e12520
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2023-11-29 08:53:05 +0000

    support

--- howdy.rb
+++ howdy.rb
@@ -1,4 +1,22 @@
+require "active_support/all"
+
 my_string = "Hello, world!"
 pp my_string
 
 require  "./goodbye.rb"
+
+pp 1.ordinalize
+pp 2.ordinalize
+pp 53.ordinalize
+pp 2009.ordinalize
+pp -21.ordinalize
+pp -134.ordinalize
+
+
+pp "table".pluralize
+pp "ruby".pluralize
+pp "equipment".pluralize
+
+pp "tables".singularize
+pp "rubies".singularize
+pp "equipment".singularize
